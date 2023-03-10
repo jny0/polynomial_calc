149bcef3f4898770850cfa00467cff905baa01a1
jny0 <pjy100402@naver.com>
2023-03-10 15:39:18 +0900

    더하기

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -5,3 +5,15 @@ public class Main {
         System.out.println("Hello world!");
     }
 }
+
+class Calc{
+    public static int run(String exp){
+        String[] bits = exp.split(" \\+ ");
+
+        int a = Integer.parseInt(bits[0]);
+        int b = Integer.parseInt(bits[1]);
+
+        return a + b;
+    }
+
+}
