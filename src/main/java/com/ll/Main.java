540a90ecb980f96c9a3855237a24561c4fcf08a0
jny0 <pjy100402@naver.com>
2023-03-13 10:20:18 +0900

    곱하기

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -11,19 +11,32 @@ public class Main {
 class Calc{
     public static int run(String exp){
 
-        exp = exp.replaceAll("- ", "+ -");
+        boolean needToMulti = exp.contains("*");
+        boolean needToPlus = !needToMulti;
 
-        String[] bits = exp.split(" \\+ ");
+        if(needToPlus){
+            exp = exp.replaceAll("- ", "+ -");
 
-        int sum = 0;
+            String[] bits = exp.split(" \\+ ");
 
-        for(int i = 0; i<bits.length; i++){
-            sum += Integer.parseInt(bits[i]);
+            int sum = 0;
+
+            for(int i = 0; i<bits.length; i++){
+                sum += Integer.parseInt(bits[i]);
+            }
+            return sum;
+
+        } else if(needToMulti){
+            String[] bits = exp.split(" \\* ");
+            int sum = 1;
+
+            for(int i = 0; i<bits.length; i++){
+                sum *= Integer.parseInt(bits[i]);
+            }
+            return sum;
         }
 
-        return sum;
-
-        //throw new RuntimeException("올바른 계산식이 아닙니다.");
+        throw new RuntimeException("올바른 계산식이 아닙니다.");
     }
 
 }
